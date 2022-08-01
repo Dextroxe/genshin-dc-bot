ab5d14ba1282a0941f90a574376d5de9e6ce4cbe
dashmat <dashmathembram99@gmail.com>
2022-08-01 22:08:18 +0530

    added link

--- src/common/db.py
+++ src/common/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
-engine = create_engine("postgres://xrpjbkhuzhrvgm:c799a517c97d0ecfc50d83d405072f5a2db99827c8af27b8dd10e44385b40ea5@ec2-34-193-44-192.compute-1.amazonaws.com:5432/dauoqi06pgtrpi", future=True)
+# engine = create_engine("postgres://xrpjbkhuzhrvgm:c799a517c97d0ecfc50d83d405072f5a2db99827c8af27b8dd10e44385b40ea5@ec2-34-193-44-192.compute-1.amazonaws.com:5432/dauoqi06pgtrpi", future=True)
+engine = create_engine("postgres+psycopg2://xrpjbkhuzhrvgm:c799a517c97d0ecfc50d83d405072f5a2db99827c8af27b8dd10e44385b40ea5@ec2-34-193-44-192.compute-1.amazonaws.com:5432/dauoqi06pgtrpi", future=True)
 session_factory = sessionmaker(bind=engine)
 session: Session = scoped_session(session_factory)()
