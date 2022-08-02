f7466febd774634958c185e17889edd7f6f37fb7
dashmat <dashmathembram99@gmail.com>
2022-08-02 09:48:04 +0530

    fixed db url

--- src/common/db.py
+++ src/common/db.py
@@ -2,8 +2,8 @@
 from sqlalchemy import create_engine, false
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
-# engine = create_engine("postgres://xrpjbkhuzhrvgm:c799a517c97d0ecfc50d83d405072f5a2db99827c8af27b8dd10e44385b40ea5@ec2-34-193-44-192.compute-1.amazonaws.com:5432/dauoqi06pgtrpi", future=True)
-engine = create_engine("postgresql+psycopg2://xrpjbkhuzhrvgm:c799a517c97d0ecfc50d83d405072f5a2db99827c8af27b8dd10e44385b40ea5@ec2-34-193-44-192.compute-1.amazonaws.com:5432/dauoqi06pgtrpi", future=True)
+# engine = create_engine("postgres://uopeepkchsqghl:9b5493488ea191ebdcdd3f513fdc3e8cb79c670ca8b8682ddb27b1815a3121ee@ec2-44-206-197-71.compute-1.amazonaws.com:5432/dev3aqeufilcc3", future=True)
+engine = create_engine("postgresql+psycopg2://uopeepkchsqghl:9b5493488ea191ebdcdd3f513fdc3e8cb79c670ca8b8682ddb27b1815a3121ee@ec2-44-206-197-71.compute-1.amazonaws.com:5432/dev3aqeufilcc3", future=True)
 session_factory = sessionmaker(bind=engine,autoflush=False)
 session: Session = scoped_session(session_factory)()
 # session_factory=session()
