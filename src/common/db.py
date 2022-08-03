fce20bf3deac7804902db3a847d412d7d49ff5be
dashmat <dashmathembram99@gmail.com>
2022-08-03 23:56:09 +0530

    db change

--- src/common/db.py
+++ src/common/db.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, false
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
-engine = create_engine("postgresql+psycopg2://mjhdlarrexrnfw:4bf4b97db8dc5d954e0ff32edd4266a9f9ab58fe16a334b404cc29a81d6a4889@ec2-3-223-242-224.compute-1.amazonaws.com:5432/dfg5ri88apl2s3", future=True)
+engine = create_engine("postgresql+psycopg2://rpthtjcezowuij:831fa4691b06b6787436122ec3ac33cf9d46d6b90af2abbc0dca990286e031e5@ec2-18-214-35-70.compute-1.amazonaws.com:5432/d2pr84p8u51i2g", future=True)
 session_factory = sessionmaker(bind=engine,autoflush=False)
 session: Session = scoped_session(session_factory)()
 # session_factory=session()
